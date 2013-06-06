adb8e56769298e626ef4389dec17946c980f78a5
Rodrigo Alves Vieira <rodrigovieira1994@gmail.com>
2013-06-06 09:50:18 -0300

    Improving User and Organization entities. Related to #37.

--- src/java/main/br/ufpe/cin/groundhog/Organization.java
+++ src/java/main/br/ufpe/cin/groundhog/Organization.java
@@ -1,11 +1,43 @@
 package br.ufpe.cin.groundhog;
 
-public class Organization {
+import java.util.Date;
 
+/**
+ * Represents a GitHub Organization in Groundhog
+ * @author gustavopinto, rodrigoalvesvieira
+ */
+public class Organization {
 	private String name;
+	private Date created_at;
+	public int public_repos;
+	public int public_gists;
+
+	public Date getCreated_at() {
+		return this.created_at;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+
+	public int getPublic_repos() {
+		return this.public_repos;
+	}
+
+	public void setPublic_repos(int public_repos) {
+		this.public_repos = public_repos;
+	}
+
+	public int getPublic_gists() {
+		return this.public_gists;
+	}
+
+	public void setPublic_gists(int public_gists) {
+		this.public_gists = public_gists;
+	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public void setName(String name) {
--- src/java/main/br/ufpe/cin/groundhog/User.java
+++ src/java/main/br/ufpe/cin/groundhog/User.java
@@ -2,27 +2,31 @@ package br.ufpe.cin.groundhog;
 
 import java.util.Date;
 
+/**
+ * Represents a GitHub User in Groundhog
+ * @author gustavopinto, rodrigoalvesvieira
+ */
 public class User {
-
-	private String login;
+	private String username;
 	private String company;
 	private String blog;
 	private boolean hirable;
 	private int followers;
 	private int following;
 	private Date created_at;
+	public int public_repos;
 	public int public_gists;
-
+	
 	public String getLogin() {
-		return login;
+		return this.username;
 	}
 
 	public void setLogin(String login) {
-		this.login = login;
+		this.username = login;
 	}
 
 	public String getCompany() {
-		return company;
+		return this.company;
 	}
 
 	public void setCompany(String company) {
@@ -30,7 +34,7 @@ public class User {
 	}
 
 	public String getBlog() {
-		return blog;
+		return this.blog;
 	}
 
 	public void setBlog(String blog) {
@@ -38,7 +42,7 @@ public class User {
 	}
 
 	public boolean isHirable() {
-		return hirable;
+		return this.hirable;
 	}
 
 	public void setHirable(boolean hirable) {
@@ -46,7 +50,7 @@ public class User {
 	}
 
 	public int getFollowers() {
-		return followers;
+		return this.followers;
 	}
 
 	public void setFollowers(int followers) {
@@ -54,7 +58,7 @@ public class User {
 	}
 
 	public int getFollowing() {
-		return following;
+		return this.following;
 	}
 
 	public void setFollowing(int following) {
@@ -62,15 +66,23 @@ public class User {
 	}
 
 	public Date getCreated_at() {
-		return created_at;
+		return this.created_at;
 	}
 
 	public void setCreated_at(Date created_at) {
 		this.created_at = created_at;
 	}
 
+	public int getPublic_repos() {
+		return this.public_repos;
+	}
+
+	public void setPublic_repos(int public_repos) {
+		this.public_repos = public_repos;
+	}
+
 	public int getPublic_gists() {
-		return public_gists;
+		return this.public_gists;
 	}
 
 	public void setPublic_gists(int public_gists) {
