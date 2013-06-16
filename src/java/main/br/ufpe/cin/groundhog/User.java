083e49aab9e9b6c09a430455dc21d4cc3789dfe6
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-06-16 09:48:04 -0300

    Adding ID and updated_at attributes for the User entity. Related to #37
    More work to come to this entity. Some basic things are still missing.

--- src/java/main/br/ufpe/cin/groundhog/User.java
+++ src/java/main/br/ufpe/cin/groundhog/User.java
@@ -4,21 +4,44 @@ import java.util.Date;
 
 /**
  * Represents a GitHub User in Groundhog
- * @author gustavopinto, rodrigoalvesvieira
+ * @author gustavopinto, Rodrigo Alves
  */
 public class User {
+	private int id;
+	
 	private String login;
 	private String email;
 	private String company;
 	private String location;
 	private String blog;
+	
 	private boolean hireable;
+	
 	private int followers;
 	private int following;
-	private Date created_at;
 	public int public_repos;
 	public int public_gists;
 	
+	private Date created_at;
+	private Date updated_at;
+	
+	/**
+	 * Informs the GitHub ID for the {@link User} object in question
+	 * This ID is unique in GitHub, which means no two users can have the same ID on GitHub
+	 * @return the integer ID
+	 */
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+    /**
+     * Informs the login (username) of the {@link User}
+     * @return
+     */
 	public String getLogin() {
 		return this.login;
 	}
@@ -75,6 +98,14 @@ public class User {
 		this.created_at = created_at;
 	}
 
+	public Date getUpdatedAt() {
+		return this.updated_at;
+	}
+
+	public void setUpdatedAt(Date updated_at) {
+		this.updated_at = updated_at;
+	}
+	
 	public int getPublic_repos() {
 		return this.public_repos;
 	}
