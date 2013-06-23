53de680b5a75c6c4a69c47ab89ed52d84659d7ac
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-06-23 11:17:14 -0300

    Adding programming language representation to Groundhog. Begins #39

---
+++ src/java/main/br/ufpe/cin/groundhog/Language.java
@@ -0,0 +1,43 @@
+package br.ufpe.cin.groundhog;
+
+/**
+ * Represents languages - an important set of components of a {@link Project} - in Groundhog.
+ * This class is only important/meaningful in the context of Projects. As a representation of 
+ * the programming language composition of such objects.
+ * 
+ * @author Rodrigo Alves
+ *
+ */
+public class Language {
+	private String name;
+	private int loc;
+	
+	public Language(String name, int loc) {
+		this.name = name;
+		this.loc = loc;
+	}
+
+	/**
+	 * Informs the language name
+	 * @return a {@link String} object
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Informs the loc (lines of code) of the language in its project
+	 *  @return a {@link String} object
+	 */
+	public int getLoc() {
+		return this.loc;
+	}
+
+	public void setLoc(int loc) {
+		this.loc = loc;
+	}
+}
