d7c720a77328e51f53b8b6c765c7179afee08889
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-08-28 14:54:03 -0300

    Removing unnecessary class inclusion

--- src/java/main/br/ufpe/cin/groundhog/main/TestMain.java
+++ src/java/main/br/ufpe/cin/groundhog/main/TestMain.java
@@ -12,10 +12,8 @@ import java.util.concurrent.Future;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import br.ufpe.cin.groundhog.Commit;
 import br.ufpe.cin.groundhog.Project;
 import br.ufpe.cin.groundhog.SCM;
-import br.ufpe.cin.groundhog.User;
 import br.ufpe.cin.groundhog.codehistory.CodeHistoryModule;
 import br.ufpe.cin.groundhog.codehistory.GitCodeHistory;
 import br.ufpe.cin.groundhog.codehistory.SFCodeHistory;
