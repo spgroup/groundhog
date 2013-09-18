21b81ff715f69de4c5a4af660d329de4c137e6d7
Jesus Jackson Sena da Silva <jesus.jackson.sena@gmail.com>
2013-09-18 15:17:50 -0300

    commit right before used a invalid token, so i'm putting my token back here

--- src/java/main/br/ufpe/cin/groundhog/http/HttpModule.java
+++ src/java/main/br/ufpe/cin/groundhog/http/HttpModule.java
@@ -16,6 +16,6 @@ public class HttpModule extends AbstractModule {
 		bind(Requests.class).in(Singleton.class);
 		bind(String.class)
 				.annotatedWith(Names.named("githubOauthToken"))
-				.toInstance("access_token=ead4492faba78faaf5d2d5732008b080304cbb23");
+				.toInstance("access_token=b0244ac0d27f7e0c227cedbd557d875869a59111");
 	}
 }
