82f2728c55ee93991bf71996cd8c816d11bcf148
Tomer Simis <tdl.simis@gmail.com>
2013-10-20 12:12:56 -0200

    Commented the getters and setters and recreated the constructor on the Release class

--- src/java/main/br/ufpe/cin/groundhog/Release.java
+++ src/java/main/br/ufpe/cin/groundhog/Release.java
@@ -37,17 +37,18 @@ public class Release implements GitHubEntity {
 
 	private Project project; 
 	
-	public Release(Project project, int id) {
+	
+	public Release(Project project, String tagName) {
 		this.project = project;
-		this.id = id;
+		this.tagName = tagName;
 	}
 
 
-
-	/*Get's and Set's bellow*/
-
-	/*lack comments to Get's and Set's bellow*/
-
+	/**
+	 * Returns the ID of the Release on GitHub. This ID is unique for every Release on GitHub, which means that
+	 * no two (or more) Issues on GitHub may have the same ID
+	 * @return
+	 */
 	public int getId() {
 		return this.id;
 	}
@@ -56,78 +57,125 @@ public class Release implements GitHubEntity {
 		this.id = id;
 	}
 
+	
+	
+	/**
+	 * Returns the Tag Name of the Release on GitHub.
+	 * @return the tag_name
+	 */
 	public String getTagName() {
-		return this.tagName;
+		return tagName;
 	}
 
-	public void setTagName(String tagName) {
+
+	public void setTag_name(String tagName) {
 		this.tagName = tagName;
 	}
 
+
+	/**
+	 * Returns the commitish value that determines where the Git tag is created from. Can be any branch or commit SHA. Defaults to the repository’s default branch (usually “master”). Unused if the Git tag already exists.
+	 * @return the target_commitish
+	 */
 	public String getTargetCommitish() {
-		return this.targetCommitish;
+		return targetCommitish;
 	}
 
-	public void setTargetCommitish(String targetCommitish) {
+	public void setTarget_commitish(String targetCommitish) {
 		this.targetCommitish = targetCommitish;
 	}
 
+
+	/**
+	 * Informs the Project object to which the Release belongs
+	 * No Release exists without a project
+	 * @return the project
+	 */
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+
+	/**
+	 * Returns the name of the Release on GitHub.
+	 * @return the name
+	 */
 	public String getName() {
-		return this.name;
+		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+
+	/**
+	 * Returns the Markdown-syntax-based description of the Release
+	 * @return the body
+	 */
 	public String getBody() {
-		return this.body;
+		return body;
 	}
 
 	public void setBody(String body) {
 		this.body = body;
 	}
 
+
+	/**
+	 * Returns true if the Release is a draft (unpublished) or false if it's published
+	 * @return the draft
+	 */
 	public boolean isDraft() {
-		return this.draft;
+		return draft;
 	}
 
 	public void setDraft(boolean draft) {
 		this.draft = draft;
 	}
 
+
+	/**
+	 * Returns true if the Release is a prerelease or false if the Release is a full release
+	 * @return the prerelease
+	 */
 	public boolean isPrerelease() {
-		return this.prerelease;
+		return prerelease;
 	}
 
 	public void setPrerelease(boolean prerelease) {
 		this.prerelease = prerelease;
 	}
 
+
+	/**
+	 * Returns the date when the Release was created
+	 * @return the createdAt
+	 */
 	public Date getCreatedAt() {
-		return this.createdAt;
+		return createdAt;
 	}
 
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
 
+
+	/**
+	 * Returns the date when the Release was published
+	 * @return the published_at
+	 */
 	public Date getPublishedAt() {
-		return this.publishedAt;
+		return publishedAt;
 	}
 
 	public void setPublishedAt(Date publishedAt) {
 		this.publishedAt = publishedAt;
 	}
-
-	public Project getProject() {
-		return this.project;
-	}
-
-	public void setProject(Project project) {
-		this.project = project;
-	}
-	/*End get and set*/
 
 	
 	@Override
