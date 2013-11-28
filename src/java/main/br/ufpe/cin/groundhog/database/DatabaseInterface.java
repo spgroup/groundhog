72a1252d59133e42935831cf0773cecbbd787965
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-11-28 15:41:57 -0300

    Adding Jongo as DB ORM starting #63

---
+++ src/java/main/br/ufpe/cin/groundhog/database/DatabaseInterface.java
@@ -0,0 +1,12 @@
+package br.ufpe.cin.groundhog.database;
+
+/**
+ * This class represents the database interface for Groundhog data persistence via MongoDB
+ * Read more at https://github.com/spgroup/groundhog#database-support
+ * 
+ * @author Rodrigo Alves
+ * 
+ */
+public class DatabaseInterface {
+
+}
